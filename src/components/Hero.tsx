e5dc8db0f9e81b127736a2a249f3a7d814e84eaa
swastisingh2 <swastisingh108@gmail.com>
2025-08-05 18:04:17 -0500

    4th try

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,7 +18,8 @@ const Hero = () => {
             <img
               src={headshotImage}
               alt="Professional headshot"
-              className="w-full h-full object-contain object-top rounded-full shadow-elegant border-4 border-accent"
+              className="w-full h-full object-contain object-center rounded-full shadow-elegant border-4 border-accent"
+
             />
             <div className="absolute inset-0 rounded-full bg-gradient-primary opacity-10"></div>
           </div>
